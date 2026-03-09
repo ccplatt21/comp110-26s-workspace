9973e022ef1441e9b815c76ce442e45c0f08f6f1
Colby Plattner <ccplatt@unc.edu>
2026-03-09 17:49:01 -0400

    final 103/100 dictionary

--- exercises/ex05/dictionary.py
+++ exercises/ex05/dictionary.py
@@ -1,34 +1,34 @@
-"""Dictionary utility functions assignment"""
+"""Dictionary utility functions assignment."""
 
 __author__ = "730672284"
 
 
-def invert(input: dict[str, str]) -> dict[str, str]:
-    """Inverts the keys and values of a given dictionary"""
+def invert(input_dict: dict[str, str]) -> dict[str, str]:
+    """Invert the keys and values of a given dictionary."""
     # initialize a variable empty list
-    Result: dict[str, str] = {}
+    result: dict[str, str] = {}
 
     # Now loop through each of the key/value pairs
-    for key in input:
-        value = input[key]  # find the value pair for the key
-        if value in Result:
+    for key in input_dict:
+        value = input_dict[key]  # find the value pair for the key
+        if value in result:
             raise KeyError(
-                "duplicate key found"
+                "Duplicate key found."
             )  # we raise the Key error if the key is found twice
         # need to invert the key value pair by using the result variable
         else:
-            Result[value] = key
+            result[value] = key
 
-    return Result
+    return result
 
 
-def favorite_color(input: dict[str, str]) -> str:
-    """Takes names and fav color and returns most popular fav color"""
+def favorite_color(input_dict: dict[str, str]) -> str:
+    """Return most popular fav color from name and fav color dict."""
     # need to create a dictionary to count the number of values
 
     count: dict[str, int] = {}
-    for name in input:
-        color: str = input[name]  # define color as the value pair to name
+    for name in input_dict:
+        color: str = input_dict[name]  # define color as the value pair to name
         if color in count:
             count[color] += 1
         else:
@@ -48,40 +48,41 @@ def favorite_color(input: dict[str, str]) -> str:
 
 
 def count(input_list: list[str]) -> dict[str, int]:
-    """taking a list and turning it into a dictionary of frequencies the strings appear"""
+    """Make a dictionary of the counts of each item in the input list."""
     # establish an empty dictionary to count up all the values
-    Result: dict[str, int] = {}
+    result: dict[str, int] = {}
     for item in input_list:
-        if item in Result:
-            Result[item] += 1
+        if item in result:
+            result[item] += 1
         else:
-            Result[item] = 1
+            result[item] = 1
 
-    return Result
+    return result
 
 
 def alphabetizer(input_list: list[str]) -> dict[str, list[str]]:
-    """takes a list of strings and sorts them by letter in alphabet"""
-    Result: dict[str, list[str]] = {}
+    """Return a list of strings and sort them by letter in alphabet."""
+    result: dict[str, list[str]] = {}
 
     # loop through the different words in the list
     for word in input_list:
         # need to get the first letter of the word and make it lowercase
         first_letter: str = word[0].lower()
         # use an if/else to see if its already in our dictionary, if not, new word
-        if first_letter in Result:
-            Result[first_letter].append(word)
+        if first_letter in result:
+            result[first_letter].append(word)
         else:
-            Result[first_letter] = [word]  # brakcets around word to create a new list
+            result[first_letter] = [word]  # brakcets around word to create a new list
 
-    return Result
+    return result
 
-    def update_attendance(
-        attendance: dict[str, list[str]], day: str, student: str
-    ) -> None:
-        """Add new students to an existing list given the day and student"""
-        # first check if the day is in the dictionary yet
-        if day in attendance:  # if yes, add student to the existing attendance
+
+def update_attendance(attendance: dict[str, list[str]], day: str, student: str) -> None:
+    """Add new students to an existing list given the day and student."""
+    # first check if the day is in the dictionary yet
+    if day in attendance:  # if yes, add student to the existing attendance
+        # check for duplicates
+        if student not in attendance[day]:
             attendance[day].append(student)
-        else:  # if not, make a new list with just that student
-            attendance[day] = [student]
+    else:  # if not, make a new list with just that student
+        attendance[day] = [student]
